58bbae484b49eb304ccd233ceb7d1b37989d976f
waskidin <113876335+waskidin@users.noreply.github.com>
2022-09-28 01:05:35 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends Cl_Controller
+{
+    public function index()
+}
+       echo "<hi>perkenalkan</hi>";
+       echo"Nama saya waskidin
+             saya tinggal di jawa barat
+             saya hobi nya ngoding dan 
+             futsal";
